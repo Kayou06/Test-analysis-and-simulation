6c7b426ca0823fc336ea8888dff989d4fb136ad9
Dromera555 <ID+YourUsername@users.noreply.github.com>
2026-03-03 16:07:57 +0100

    Added displaying multiple fields

--- OF_plot.py
+++ OF_plot.py
@@ -41,3 +41,59 @@ def draw_quiver(u,v,beforeImg):
 
     plt.draw()
     plt.show()
+
+
+def draw_quiver_many(u, v, beforeImg, titles=None, *, scale=10, step=8):
+    many = isinstance(u, (list, tuple))
+
+    if not many:
+        us = [u]
+        vs = [v]
+        imgs = [beforeImg]
+    else:
+        us = list(u)
+        vs = list(v)
+        imgs = list(beforeImg)
+
+    n = len(us)
+    if len(vs) != n or len(imgs) != n:
+        raise ValueError("u, v, beforeImg must have the same number of items")
+
+    if titles is None:
+        titles_list = None
+    elif isinstance(titles, str):
+        titles_list = [titles] + [None] * (n - 1)
+    else:
+        if len(titles) != n:
+            raise ValueError("titles must have the same length as the number of fields")
+        titles_list = list(titles)
+
+    fig, axs = plt.subplots(1, n, figsize=(6 * n, 6))
+    if n == 1:
+        axs = [axs]
+
+    for k in range(n):
+        ax = axs[k]
+        uu = us[k]
+        vv = vs[k]
+        img = imgs[k]
+
+        ax.imshow(img, cmap="gray")
+
+        magnitudeAvg = get_magnitude(uu, vv)
+
+        for i in range(0, uu.shape[0], step):
+            for j in range(0, uu.shape[1], step):
+                dy = vv[i, j]
+                dx = uu[i, j]
+                magnitude = (dx**2 + dy**2)**0.5
+                if magnitude > magnitudeAvg:
+                    ax.arrow(j, i, dx * scale, dy * scale, color="red")
+
+        if titles_list is not None and titles_list[k] is not None:
+            ax.set_title(titles_list[k])
+
+        ax.set_axis_off()
+
+    plt.tight_layout()
+    plt.show()
--- display_field.py
+++ display_field.py
@@ -1,8 +1,8 @@
-def display_field():
-    import numpy as np
-    import cv2 as cv
-    from OF_plot import draw_quiver
+import numpy as np
+import cv2 as cv
+from OF_plot import draw_quiver, draw_quiver_many
 
+def display_field():
     u = np.load("u_HS.npy")
     v = np.load("v_HS.npy")
 
@@ -11,4 +11,22 @@ def display_field():
     draw_quiver(u, v, beforeImg)
 
 
-display_field()
+def display_many_fields():
+    u1 = np.load("u_HS.npy")
+    v1 = np.load("v_HS.npy")
+    img1 = cv.imread("Correlable_pics/BOS_12_11_ref_masked.tif", cv.IMREAD_GRAYSCALE)
+
+    u2 = np.load("u_HS_alpha10_blur1.npy")
+    v2 = np.load("v_HS_alpha10_blur1.npy")
+    img2 = cv.imread("Correlable_pics/BOS_12_11_ref_masked.tif", cv.IMREAD_GRAYSCALE)
+
+    draw_quiver_many(
+        [u1, u2],
+        [v1, v2],
+        [img1, img2],
+        titles=["Run 1", "Run 2"]
+    )
+
+
+if __name__ == "__main__":
+    display_many_fields()
