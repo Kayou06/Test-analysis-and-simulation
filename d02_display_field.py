5db8ca952267e66253120bfba0f2b23d62b13e5a
lisanne-b <l.m.l.boer@student.tudelft.nl>
2026-03-05 11:48:15 +0100

    changes in display field function and main file

--- d02_display_field.py
+++ d02_display_field.py
@@ -13,43 +13,21 @@ def display_field():
     draw_quiver(u, v, beforeImg)
 
 
-def display_many_fields():
-    root = "VF BOS_12_11_1 (220)/"
+def display_many_fields(u1_path, v1_path, u2_path, v2_path):
 
-    u1 = np.load(root + "u_HS_alpha5_blur5.npy")
-    v1 = np.load(root + "v_HS_alpha5_blur5.npy")
+    u1 = np.load(u1_path)
+    v1 = np.load(v1_path)
     img1 = cv.imread("Correlable_pics/BOS_12_11_ref_masked.tif", cv.IMREAD_GRAYSCALE)
 
-    u2 = np.load(root + "u_HS_alpha10_blur5.npy")
-    v2 = np.load(root + "v_HS_alpha10_blur5.npy")
+    u2 = np.load(u2_path)
+    v2 = np.load(v2_path)
     img2 = cv.imread("Correlable_pics/BOS_12_11_ref_masked.tif", cv.IMREAD_GRAYSCALE)
 
-    u3 = np.load(root + "u_HS_alpha15_blur5.npy")
-    v3 = np.load(root + "v_HS_alpha15_blur5.npy")
-    img3 = cv.imread("Correlable_pics/BOS_12_11_ref_masked.tif", cv.IMREAD_GRAYSCALE)
-
-    u4 = np.load(root + "u_HS_alpha20_blur5.npy")
-    v4 = np.load(root + "v_HS_alpha20_blur5.npy")
-    img4 = cv.imread("Correlable_pics/BOS_12_11_ref_masked.tif", cv.IMREAD_GRAYSCALE)
-
-    u5 = np.load(root + "u_HS_alpha25_blur5.npy")
-    v5 = np.load(root + "v_HS_alpha25_blur5.npy")
-    img5 = cv.imread("Correlable_pics/BOS_12_11_ref_masked.tif", cv.IMREAD_GRAYSCALE)
-
-    u6 = np.load(root + "u_HS_alpha30_blur5.npy")
-    v6 = np.load(root + "v_HS_alpha30_blur5.npy")
-    img6 = cv.imread("Correlable_pics/BOS_12_11_ref_masked.tif", cv.IMREAD_GRAYSCALE)
-
-    u7 = np.load(root + "u_HS_alpha35_blur5.npy")
-    v7 = np.load(root + "v_HS_alpha35_blur5.npy")
-    img7 = cv.imread("Correlable_pics/BOS_12_11_ref_masked.tif", cv.IMREAD_GRAYSCALE)
-
-
     draw_quiver_many(
-        [u1, u2, u3, u4, u5, u6, u7],
-        [v1, v2, v3, v4, v5, v6, v7],
-        [img1, img2, img3, img4, img5, img6, img7],
-        titles=["Run 1", "Run 2", "Run 3", "Run 4", "Run 5", "Run 6", "Run 7"]
+        [u1, u2],
+        [v1, v2],
+        [img1, img2],
+        titles=["Run 1", "Run 2"]
     )
 
 
