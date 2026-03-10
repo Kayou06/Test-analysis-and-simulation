2b58de195b6df13900f4a721a389791c42220ab9
Dromera555 <ID+YourUsername@users.noreply.github.com>
2026-03-10 15:23:38 +0100

    Made function to correct vectors outside of mask

--- d02_field_corrections.py
+++ d02_field_corrections.py
@@ -1,36 +1,24 @@
 import numpy as np
-import cv2 as cv
-from d02_display_field import display_many_fields
 import math
 
-u = np.load("u_HS.npy")
-v = np.load("v_HS.npy") # V is upwards, U is rightwards
-mask = cv.imread("Correlable_pics/BOS_12_11_ref_masked.tif", cv.IMREAD_GRAYSCALE)
+def mask_correction(u, v, mask):
+    # Index i is the y axis in the graph. Index j is the x axis in the graph.
+    for i, row in enumerate(mask):
+        for j, pixel in enumerate(row):
+            if mask[i][j] == 255:
+                u[i][j] = 0
+                v[i][j] = 0
+            
+            new_i = i + math.ceil(v[i][j]*10)
+            new_j = j + math.ceil(u[i][j]*10)
 
-k = 0
-for i, row in enumerate(mask):
-    for j, pixel in enumerate(row):
-        if mask[i][j] == 255:
-            u[i][j] = 0
-            v[i][j] = 0
-        
-        new_i = i + math.ceil(v[i][j])
-        new_j = j + math.ceil(u[i][j])
+            try:   
+                if mask[new_i][new_j] == 255:
+                    u[i][j] = 0
+                    v[i][j] = 0
+            except IndexError:
+                pass
 
-        if mask[new_i][new_j] == 255:
-            u[i][j] = 0
-            v[i][j] = 0
-            k+=1
+    np.save("u_corr", u)
+    np.save("v_corr", v)
 
-
-# Index i is the y axis in the graph. Index j is the x axis in the graph.
-
-np.save("u_corr", u)
-np.save("v_corr", v)
-
-u_or = np.load("u_HS.npy")
-v_or = np.load("v_HS.npy")
-
-display_many_fields([("u_corr.npy", "v_corr.npy", "Correlable_pics/BOS_12_11_ref_masked.tif", "Corrected Field"), ("u_HS.npy", "v_HS.npy", "Correlable_pics/BOS_12_11_ref_masked.tif", "Original Field")])
-
-print(k)
