aa94d00fc121eb8c099861d336de9de437c47780
Dromera555 <ID+YourUsername@users.noreply.github.com>
2026-03-26 09:46:57 +0100

    Added full density gradient graph for CC

--- D02_CC_densitygrad.py
+++ D02_CC_densitygrad.py
@@ -34,7 +34,11 @@ corr_files = [
 ]
 
 
-def calc_drho_dx(del_x, rho):
+import numpy as np
+import pandas as pd
+
+
+def calc_drho_d(displacement, rho):
     C = 5.3
     ZD = 0.010
     ZA = 1.250
@@ -42,7 +46,36 @@ def calc_drho_dx(del_x, rho):
     W = 0.020
     K = 4.5e-4
     n0 = K * rho + 1
-    return del_x * n0 * (ZD + ZA - f) / (C * W * K * f * ZD)
+    return displacement * n0 * (ZD + ZA - f) / (C * W * K * f * ZD)
+
+
+def extract_all_displacements(bos_file, corr_file):
+    df_BOS = pd.read_csv(bos_file, delimiter=';')
+    df_corr = pd.read_csv(corr_file, delimiter=';')
+
+    x = df_BOS['x'].to_numpy()
+    y = df_BOS['y'].to_numpy()
+    u = df_BOS['x-displacement'].to_numpy()
+    v = df_BOS['y-displacement'].to_numpy()
+
+    u_corr = df_corr['x-displacement'].mean()
+    v_corr = df_corr['y-displacement'].mean()
+
+    u_final = u - u_corr
+    v_final = v - v_corr
+
+    return x, y, u_final, v_final
+
+
+def calc_density_gradient_all_points(bos_file, corr_file, rho):
+    x, y, u_final, v_final = extract_all_displacements(bos_file, corr_file)
+
+    drho_dx = calc_drho_d(u_final, rho)
+    drho_dy = calc_drho_d(v_final, rho)
+
+    density_gradient = np.sqrt(drho_dx**2 + drho_dy**2)
+
+    return x, y, density_gradient
 
 
 def extract_midline_displacement(bos_file, corr_file):
@@ -81,7 +114,7 @@ def extract_midline_displacement(bos_file, corr_file):
 
     return midline_df
 
-
+'''
 # -------------------------------------------------
 # Plot drho/dx for all 7 files
 # -------------------------------------------------
@@ -124,3 +157,28 @@ plt.title('Normalized drho/dx vs x for 7 BOS files')
 plt.grid(True)
 plt.legend()
 plt.show()
+'''
+
+# Full Density Gradient Plot
+
+x, y, density_gradient = calc_density_gradient_all_points(
+    bos_files[0],
+    corr_files[0],
+    rho0[0]
+)
+
+fig, ax = plt.subplots(figsize=(16, 4))
+sc = ax.scatter(x, y, c=density_gradient, s=10)
+
+ax.set_xlabel(r"$x$ [$m$]")
+ax.set_ylabel(r"$y$ [$m$]")
+ax.set_title("Density Gradient")
+ax.set_aspect('equal', adjustable='box')
+
+cbar = fig.colorbar(sc, ax=ax, orientation='horizontal', pad=0.25, fraction=0.1, aspect=60)
+cbar.set_label(r"[$kg/m^2$]")
+
+fig.savefig("Density_Gradient.png", dpi=300, bbox_inches="tight")
+plt.show()
+plt.close(fig)
+
